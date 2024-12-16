8f54165a213793003b0713d42916e8ceae4a6eed
AntonOsipov99 <antonosipov@gmx.de>
2024-12-16 23:58:02 +0100

    add data to admin panel

--- join_backend_app/admin.py
+++ join_backend_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Contacts, AllTasks
 
-# Register your models here.
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'contacts')
+
+@admin.register(AllTasks)
+class AllTasksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'allTasks') 
