a5aeb76abe5b7ac3614145aa70c634cabe486c6b
AntonOsipov99 <antonosipov@gmx.de>
2024-12-20 02:01:22 +0100

    change admin panel

--- join_backend_app/admin.py
+++ join_backend_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from django import forms
 from .models import Contact, Task
+import datetime
+
 
 @admin.register(Contact)
 class ContactsAdmin(admin.ModelAdmin):
@@ -7,15 +10,37 @@ class ContactsAdmin(admin.ModelAdmin):
     list_filter = ('color',)
     search_fields = ('name', 'email', 'phone')
 
+
+class TaskAdminForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        widgets = {
+            'created_at': forms.DateInput(attrs={'type': 'date', 'min': datetime.date.today().strftime('%Y-%m-%d')})
+        }
+
+
 @admin.register(Task)
 class TasksAdmin(admin.ModelAdmin):
+    form = TaskAdminForm
     list_display = (
-        'id', 
-        'title', 
-        'task_category', 
-        'priority', 
-        'created_at', 
-        'in_which_container'
+        'id',
+        'title',
+        'task_category',
+        'priority',
+        'created_at',
     )
     list_filter = ('task_category', 'priority', 'created_at')
     search_fields = ('title', 'description_text', 'task_id')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title',
+                'task_category',
+                'category_color',
+                'description_text',
+                'created_at',
+            ),
+        }),
+    )
